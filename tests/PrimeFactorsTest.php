dd6e146d433c2f1719effa0fc25392b1836b64d2
Sebastian Bergmann <sb@sebastian-bergmann.de>
2014-03-13 10:26:24 +0100

    Step 2
    phpunit:
         [exec] PHPUnit 4.0.7 by Sebastian Bergmann.
         [exec]
         [exec] Configuration read from /usr/local/src/phpunit-kata-prime-factors/build/phpunit.xml
         [exec]
         [exec] PHP Fatal error:  Call to undefined method PrimeFactors::generate() in /usr/local/src/phpunit-kata-prime-factors/tests/PrimeFactorsTest.php on line 16
         [exec] PHP Stack trace:
         [exec] PHP   1. {main}() /usr/local/src/phpunit-kata-prime-factors/build/tools/phpunit.phar:0
         [exec] PHP   2. PHPUnit_TextUI_Command::main() /usr/local/src/phpunit-kata-prime-factors/build/tools/phpunit.phar:581
         [exec] PHP   3. PHPUnit_TextUI_Command->run() phar:///usr/local/src/phpunit-kata-prime-factors/build/tools/phpunit.phar/phpunit/TextUI/Command.php:132
         [exec] PHP   4. PHPUnit_TextUI_TestRunner->doRun() phar:///usr/local/src/phpunit-kata-prime-factors/build/tools/phpunit.phar/phpunit/TextUI/Command.php:179
         [exec] PHP   5. PHPUnit_Framework_TestSuite->run() phar:///usr/local/src/phpunit-kata-prime-factors/build/tools/phpunit.phar/phpunit/TextUI/TestRunner.php:419
         [exec] PHP   6. PHPUnit_Framework_TestSuite->run() phar:///usr/local/src/phpunit-kata-prime-factors/build/tools/phpunit.phar/phpunit/Framework/TestSuite.php:722
         [exec] PHP   7. PHPUnit_Framework_TestCase->run() phar:///usr/local/src/phpunit-kata-prime-factors/build/tools/phpunit.phar/phpunit/Framework/TestSuite.php:722
         [exec] PHP   8. PHPUnit_Framework_TestResult->run() phar:///usr/local/src/phpunit-kata-prime-factors/build/tools/phpunit.phar/phpunit/Framework/TestCase.php:764
         [exec] PHP   9. PHPUnit_Framework_TestCase->runBare() phar:///usr/local/src/phpunit-kata-prime-factors/build/tools/phpunit.phar/phpunit/Framework/TestResult.php:692
         [exec] PHP  10. PHPUnit_Framework_TestCase->runTest() phar:///usr/local/src/phpunit-kata-prime-factors/build/tools/phpunit.phar/phpunit/Framework/TestCase.php:824
         [exec] PHP  11. ReflectionMethod->invokeArgs() phar:///usr/local/src/phpunit-kata-prime-factors/build/tools/phpunit.phar/phpunit/Framework/TestCase.php:956
         [exec] PHP  12. PrimeFactorsTest->testOne() phar:///usr/local/src/phpunit-kata-prime-factors/build/tools/phpunit.phar/phpunit/Framework/TestCase.php:956
    
    BUILD FAILED

--- tests/PrimeFactorsTest.php
+++ tests/PrimeFactorsTest.php
@@ -1,4 +1,18 @@
 <?php
 class PrimeFactorsTest extends PHPUnit_Framework_TestCase
 {
+    /**
+     * @var PrimeFactors
+     */
+    private $primeFactors;
+
+    protected function setUp()
+    {
+        $this->primeFactors = new PrimeFactors;
+    }
+
+    public function testOne()
+    {
+        $this->assertEquals(array(), $this->primeFactors->generate(1));
+    }
 }
